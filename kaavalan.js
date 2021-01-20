cb033d459e6e4023b01ad00cd5ba4a61808a06c8
JAGAGANESH <67314150+JAGAGANESH@users.noreply.github.com>
2021-01-20 08:51:14 +0530

    Update kaavalan.js

--- kaavalan.js
+++ kaavalan.js
@@ -1,15 +1,3 @@
-var config = {
-apiKey: "AIzaSyAB_Q9APAALhTFv3djz9cMxft5uft2LwCM",
-authDomain: "duja-55555.firebaseapp.com",
-databaseURL: "https://duja-55555-default-rtdb.firebaseio.com",
-projectId: "duja-55555",
-storageBucket: "duja-55555.appspot.com",
-messagingSenderId: "203703988385",
-appId: "1:203703988385:web:1774dba54125cae6b980e0",
-    measurementId: "G-EVFS9WQZ9F"
-      };
-      firebase.initializeApp(config);
-       firebase.analytics();
 function kaavalan() {
 	var user = firebase.auth().currentUser;
 	if (user == null) {
