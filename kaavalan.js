017a231c3561a19ffead70ec1e7e41b27d0f4564
JAGAGANESH <67314150+JAGAGANESH@users.noreply.github.com>
2021-01-20 09:26:21 +0530

    Update kaavalan.js

--- kaavalan.js
+++ kaavalan.js
@@ -1,9 +1,9 @@
 function kaavalan() {
 	var user = firebase.auth().currentUser;
-	if (user == null) {
-  		alert("நீங்கள் துஜ அல்ல !"+user);
+	//if (user == null) {
+  	//	alert("நீங்கள் துஜ அல்ல !"+user);
 		//location.replace("index.html");
-	}
+	//}
 	var kaavalan = user.uid;
 	if(kaavalan != "ALbZ2dAEj0XJHLS3797w01YQq0x2") {
 		alert("நீங்கள் துஜ அல்ல !"+kaavalan);
